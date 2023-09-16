7ed23b4f69c4e986b2b5dfd09369375a20dbdc4d
jangdu <jjd0324@gmail.com>
2023-09-17 01:44:58 +0900

    fix: repository 관련 di 수정

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostsRepository } from './posts.repository';
+import { Post } from 'src/entities/post.entity';
+import { Comment } from 'src/entities/comment.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, Comment])],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService, PostsRepository],
 })
 export class PostsModule {}
