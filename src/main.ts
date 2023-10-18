3183fc3ec2984c7fdbcc61978f49550a96e310ae
jangdu <jjd0324@gmail.com>
2023-10-18 11:57:38 +0900

    refactor: 의미없는 주석 제거

--- src/main.ts
+++ src/main.ts
@@ -23,16 +23,12 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://jangdu.site-front.s3-website.ap-northeast-2.amazonaws.com/',
     ],
-    credentials: true, // 요청에 쿠키 정보를 함께 보냄
+    credentials: true,
   });
 
   const port = process.env.PORT || 3000;
 
-  // class validator 전역 적용
   app.useGlobalPipes(new ValidationPipe());
-  // // httpExceptionfilter활성화 예외처리 전역 필터 설정
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  // cors 설정
 
   await app.listen(port);
 
